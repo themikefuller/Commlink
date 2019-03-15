9332212914a92094d37eaa53d8052c2dbc8c3d66
Mike Fuller <emailthemike@gmail.com>
2019-03-14 20:33:49 -0400

    updated test for rewrite

--- test.js
+++ test.js
@@ -4,4 +4,10 @@ const path = require('path');
 const Commlink = require(__dirname + path.sep + 'index.js');
 const comm = Commlink();
 
-comm.testAliceAndBob().then(console.log).catch(console.log);
+let params1 = {"chainAlg":"pbkdf2"};
+let params2 = {"chainAlg":"hkdf"};
+
+comm.test(params1).then(console.log).catch(console.log);
+
+// This test fails in nginx currently. The crypto package does not support hkdf
+//comm.test(params2).then(console.log).catch(console.log);
