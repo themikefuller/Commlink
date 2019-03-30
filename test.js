2e62fd1537e1ffe6ee5ad37a086cf81d42119cb0
Mike Fuller <emailthemike@gmail.com>
2019-03-29 23:36:43 -0400

    cleanup

--- test.js
+++ test.js
@@ -8,6 +8,5 @@ let params1 = {"chainAlg":"pbkdf2"};
 let params2 = {"chainAlg":"hkdf"};
 
 comm.test(params1).then(console.log).catch(console.log);
-
 // This test fails in nginx currently. The crypto package does not support hkdf
-//comm.test(params2).then(console.log).catch(console.log);
+// comm.test(params2).then(console.log).catch(console.log);
