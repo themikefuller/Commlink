8f067786afcc46e15e55644588178a8174a62744
Mike Fuller <emailthemike@gmail.com>
2019-07-22 22:19:56 -0400

    added test for HKDF which in turn tests hmacSign

--- test.js
+++ test.js
@@ -3,14 +3,13 @@
 const path = require('path');
 const Commlink = require(__dirname + path.sep + 'index.js');
 const comm = Commlink();
-const commlink = comm;
 
 async function Test(params={}) {
 
     let alice = {};
-    alice.idk = await commlink.createECDSA();
-    alice.spk = await commlink.createECDH();
-    alice.sig = await commlink.sign(alice.idk.key, alice.spk.pub);
+    alice.idk = await comm.createECDSA();
+    alice.spk = await comm.createECDH();
+    alice.sig = await comm.sign(alice.idk.key, alice.spk.pub);
     alice.card = {
       "idk":alice.idk.pub,
       "spk":alice.spk.pub,
@@ -18,23 +17,26 @@ async function Test(params={}) {
     };
 
     let bob = {};
-    bob.idk = await commlink.createECDSA();
-    bob.spk = await commlink.createECDH();
-    bob.sig = await commlink.sign(bob.idk.key, bob.spk.pub);
+    bob.idk = await comm.createECDSA();
+    bob.spk = await comm.createECDH();
+    bob.sig = await comm.sign(bob.idk.key, bob.spk.pub);
     bob.card = {
       "idk":bob.idk.pub,
       "spk":bob.spk.pub,
       "sig":bob.sig
     };
 
-    alice.card.verified = await commlink.verify(alice.card.idk, alice.card.sig, alice.card.spk);
-    bob.card.verified = await commlink.verify(bob.card.idk, bob.card.sig, bob.card.spk);
+    alice.card.verified = await comm.verify(alice.card.idk, alice.card.sig, alice.card.spk);
+    bob.card.verified = await comm.verify(bob.card.idk, bob.card.sig, bob.card.spk);
 
-    alice.sharedKey = await commlink.ecdh(alice.spk.key, bob.card.spk);
-    bob.sharedKey = await commlink.ecdh(bob.spk.key, alice.card.spk);
+    alice.sharedKey = await comm.ecdh(alice.spk.key, bob.card.spk);
+    bob.sharedKey = await comm.ecdh(bob.spk.key, alice.card.spk);
 
-    alice.encrypted = await commlink.encrypt("Hello Bob!", comm.decode(alice.sharedKey));
-    bob.decrypted = await commlink.decrypt(alice.encrypted, comm.decode(bob.sharedKey));
+    alice.hashKey = await comm.hkdf(comm.decode(alice.sharedKey), new Uint8Array([0]), comm.fromText("Hash"), 256, "SHA-256");
+    bob.hashKey = await comm.hkdf(comm.decode(bob.sharedKey), new Uint8Array([0]), comm.fromText("Hash"), 256, "SHA-256");
+
+    alice.encrypted = await comm.encrypt("Hello Bob!", comm.decode(alice.sharedKey));
+    bob.decrypted = await comm.decrypt(alice.encrypted, comm.decode(bob.sharedKey));
 
     return {alice, bob};
 
